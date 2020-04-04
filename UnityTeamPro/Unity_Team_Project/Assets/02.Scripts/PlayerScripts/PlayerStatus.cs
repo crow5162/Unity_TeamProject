05e07ae7ceaedb91bae0548dd8fa23197f8a565d
Minouk <crow5162@gmail.com>
2020-04-05 06:56:07 +0900

    MinWook 2020.04.05 - Commit List - 1. Character Roll Motion Add 2. Unity Particle System Package Download ( Need Pakage Manager Update) 3. Add Player Hp 4. Change ShotGun & AssultRifle Pellets Texture

---
+++ UnityTeamPro/Unity_Team_Project/Assets/02.Scripts/PlayerScripts/PlayerStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    private const string enemyBulletTag = "E_BULLET";
+    private const string enemyTag = "ENEMY";
+
+    private Animator anim;
+    private readonly int hashDeath = Animator.StringToHash("isDead");
+
+    private float maxHp = 250.0f;
+    private float currentHp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentHp = maxHp;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag == enemyBulletTag)
+        {
+            currentHp -= 20.0f;
+            Destroy(coll.gameObject);
+
+            if(currentHp <= 0)
+            {
+                OnPlayerDie();
+            }
+        }
+    }
+
+    void OnPlayerDie()
+    {
+        anim.SetBool(hashDeath, true);
+    }
+}
