b374e15c3faba95db2b3d8ad7327b2ce6965ab21
Minouk <crow5162@gmail.com>
2020-04-05 07:17:23 +0900

    MinWook 2020.04.05 - Commit 1. Enemy, Character merging 2. 몬스터파트 날아간부분 없는지 확인해주세요

--- UnityTeamPro/Unity_Team_Project/Assets/AttackCollider.cs
+++ UnityTeamPro/Unity_Team_Project/Assets/AttackCollider.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackCollider : MonoBehaviour
-{
-    public event Delegate<Collider> Enter;
-
-    public void OnTriggerEnter(Collider other)
-    {
-        Enter();
-    }
-
-}
+//public class AttackCollider : MonoBehaviour
+//{
+//    public event Delegate<Collider> Enter;
+//
+//    public void OnTriggerEnter(Collider other)
+//    {
+//        Enter();
+//    }
+//
+//}
